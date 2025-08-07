baecfe4b065ec24058e965cb552170cc08687c4f
Anjali Thakur <34119661+anjalithakur1276@users.noreply.github.com>
2025-08-07 21:26:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let count = 0;
+let count_t2 = 0;
+function increament_t1() {
+  count = count + 1;
+  document.getElementById("count").textContent = count;
+}
+
+function increament_t11() {
+  count = count + 2;
+  document.getElementById("count").textContent = count;
+}
+
+function increament_t111() {
+  count = count + 3;
+  document.getElementById("count").textContent = count;
+}
+
+function increament_t2() {
+  count_t2 = count_t2 + 1;
+  document.getElementById("count_t2").textContent = count_t2;
+}
+
+function increament_t22() {
+  count_t2 = count_t2 + 2;
+  document.getElementById("count_t2").textContent = count_t2;
+}
+
+function increament_t222() {
+  count_t2 = count_t2 + 3;
+  document.getElementById("count_t2").textContent = count_t2;
+}
